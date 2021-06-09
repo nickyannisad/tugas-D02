f1070a8e4b3d0eb55eddc1333e89d9c85fd58302
nickyannisad <85160510+nickyannisad@users.noreply.github.com>
2021-06-09 12:23:00 +0700

    Create stylesheet.css

---
+++ stylesheet.css
@@ -0,0 +1,18 @@
+.message-wrapper {
+  border-bottom: 1px solid #eee;
+  padding-bottom: 90px;
+  text-align: center;
+}
+
+.message {
+  padding: 30px 80px;
+  background-color: #0984e3;
+  cursor: pointer;
+  box-shadow: 0 10px #81ecec;
+}
+
+.message:active {
+  position: relative;
+  top: 10px;
+  box-shadow: none;
+}
